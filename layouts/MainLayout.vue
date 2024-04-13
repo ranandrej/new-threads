fe6c462114fc2287c420ff298fca5f381af9e546
unknown <ranandrej@gmail.com>
2024-04-14 01:01:59 +0200

    endddysss

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -37,5 +37,5 @@ const user=useSupabaseUser()
 </template>
 
 <style scoped>
-@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css")
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css");
 </style>
