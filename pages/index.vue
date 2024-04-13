6c1114d9f13b639723d25af1827236aed953c06d
unknown <ranandrej@gmail.com>
2024-04-14 00:02:04 +0200

    Everything ok

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+import axios from "axios"
+const text=ref("")
+onMounted(async()=>{
+    await axios.get('/api/hi').then((res)=>{
+        text.value=res.data
+    })
+    
+        
+    
+})
+
+</script>
+
+<template>
+    <h2>Hello world</h2>
+    <h3>{{ text }}</h3>
+</template>
