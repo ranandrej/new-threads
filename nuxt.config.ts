9a09975247f947dcf5464abd675b86c3d302539f
unknown <ranandrej@gmail.com>
2024-04-14 00:27:22 +0200

    Hellouuujea

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ["@nuxtjs/tailwindcss","@nuxtjs/supabase","@pinia/nuxt"]
+  modules: ["@nuxtjs/tailwindcss","@nuxtjs/supabase","@pinia/nuxt"],
+  supabase: {
+    // Options
+    redirect:false
+    
+  }
 })
